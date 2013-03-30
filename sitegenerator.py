ae771289f03445a81e3c52878a2c128d05442340
Anshuman Bansal <anotheranshu@gmail.com>
2013-03-30 04:10:46 -0400

    Windows makes me sad

--- sitegenerator.py
+++ sitegenerator.py
@@ -14,7 +14,7 @@ def index():
   return makehome(keys, dict["Course Info"])
 
 def makehome(myheaders, mydic):
-  return render_template('base.html', headers = myheaders, dict = mydic)
+  return render_template('home.html', headers = myheaders, dict = mydic)
 
 if __name__ == '__main__':
   app.run(debug=True)
