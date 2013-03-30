5d050e757a00e071c94c072ff4222af6bfac57d9
Anshuman Bansal <anotheranshu@gmail.com>
2013-03-30 09:17:25 -0400

    Adding the Homework template

--- sitegenerator.py
+++ sitegenerator.py
@@ -8,12 +8,18 @@ app = Flask(__name__)
 def index():
   (keys, dict) = parser.parse("tests/15150.wat")
   if "Course Info" in keys:
-    f = open("html/courseinfo.html", 'r+')
-    f.write(makehome(keys, dict["Course Info"]))
+    a = open("html/courseinfo.html", 'r+')
+    a.write(makehome(keys, dict["Course Info"]))
+  if "Homework" in keys:
+    b = open("html/homework.html", 'r+')
+    b.write(makehw(keys, dict["Course Info"], dict["Homework"]
   return makehome(keys, dict["Course Info"])
 
 def makehome(myheaders, mydic):
   return render_template('home.html', headers = myheaders, dict = mydic)
 
+def makehw(myheaders, mydic, mycont):
+  return render_template('homework.html', headers = myheaders, dict = mydic, cont = mycont)
+  
 if __name__ == '__main__':
   app.run(debug=True)
