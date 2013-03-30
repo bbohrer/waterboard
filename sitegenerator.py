27e5215fb14f771d70cc7723f13647f730bef348
aakpat6 <aakpat6@gmail.com>
2013-03-30 03:20:41 -0400

    Base template working

--- sitegenerator.py
+++ sitegenerator.py
@@ -2,23 +2,19 @@ import parser
 import os
 from flask import *
 
+app = Flask(__name__)
+
 @app.route('/')
 def index():
   (keys, dict) = parser.parse("tests/15150.wat")
+  print keys
   if "Course Info" in keys:
     f = open("html/courseinfo.html", 'r+')
     f.write(makehome(keys, dict["Course Info"]))
+  return makehome(keys, dict["Course Info"])
 
+def makehome(myheaders, mydic):
+  return render_template('base.html', headers = myheaders, dict = mydic)
 
 if __name__ == '__main__':
-  app.run()
-  
-def makehome(myparts, mydic):
-  return render_template("templates/home.html", parts = myparts, dict = mydic)
-
-
-(keys, dict) = parser.parse("tests/15150.wat")
-if "Course Info" in keys:
-  f = open("html/courseinfo.html", 'r+')
-  f.write(makehome(keys, dict["Course Info"]))
-
+  app.run(debug=True)
